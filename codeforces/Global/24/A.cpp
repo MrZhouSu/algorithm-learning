db48883594392310039dee0d4dab7a20343298eb
MrZhouSu <106241703+MrZhouSu@users.noreply.github.com>
2023-02-20 22:49:58 +0800

    Create A.cpp

---
+++ codeforces/Global/24/A.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>  //iota
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#define ll long long
+using namespace std;
+
+void solved() {
+  ll n;
+  cin >> n;
+  ll x;
+  for (ll i = 0; i < n; i++) {
+    cin >> x;
+  }
+  cout << 1 << " " << n << endl;
+}
+
+signed main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  ll t;
+  cin >> t;
+
+  while (t--) solved();
+
+  return (0 ^ 0);
+}

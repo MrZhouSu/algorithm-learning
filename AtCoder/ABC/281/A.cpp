06cbc545148fa929449c6f38d48a77d2e2da1171
MrZhouSu <106241703+MrZhouSu@users.noreply.github.com>
2023-02-21 20:47:50 +0800

    Create A.cpp

---
+++ AtCoder/ABC/281/A.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>  //iota
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#define ll long long
+using namespace std;
+
+signed main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  ll n;
+  cin >> n;
+  for (ll i = n; i >= 0; i--) {
+    cout << i << endl;
+  }
+
+  return (0 ^ 0);
+}
